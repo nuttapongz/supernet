5030e0f9f834c6a1432bffa255a469dd22199a39
nuttapongz <33756748+nuttapongz@users.noreply.github.com>
2018-01-04 02:39:50 +0700

    Add files via upload

---
+++ bannersshlink.sh
@@ -0,0 +1,11 @@
+#sed -i 's/INFO/INFO\nBanner bannerssh' /etc/ssh/sshd_config
+
+#replace banner dropbear
+sed -i 's/DROPBEAR_BANNER=""/DROPBEAR_BANNER="bannerssh"/g' /etc/default/dropbear
+
+# bannerssh
+wget "bannerssh.txt"
+mv ./bannerssh /bannerssh
+chmod 0644 /bannerssh
+service dropbear restart
+service ssh restart
